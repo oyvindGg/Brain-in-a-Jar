d26312cd8e6c58463b48119ac8fce570418b58ab
0yvz <oyvindg@live.no>
2023-05-23 15:41:43 +0200

    Exported schemas

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,1 +1,13 @@
-export default []
+import product from './documents/product.js'
+import page from './documents/page.js'
+import faq from './documents/faq.js'
+import testimonial from './documents/testimonial.js'
+import settings from './documents/settings.js'
+
+export default [
+	product,
+	page,
+	faq,
+	testimonial,
+	settings
+]
