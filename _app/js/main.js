3a1149f70f4c6aac75b8a3a3cf2a9f38d8aea3d2
0yvz <oyvindg@live.no>
2023-06-01 23:54:30 +0200

    Imported faq function

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,7 @@
 import titleunfocus from "./modules/titleunfocus.js";
 import shopCustomisation from "./modules/shop-customisation.js";
+import loadFAQSection from "./modules/faq.js";
 
 titleunfocus();
 shopCustomisation();
+loadFAQSection();
