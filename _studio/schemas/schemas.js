89c894a8e57b8c391999af14322147baff67c736
0yvz <oyvindg@live.no>
2023-06-02 05:18:49 +0200

    Removed settings from studio

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -2,12 +2,10 @@ import product from './documents/product.js'
 import page from './documents/page.js'
 import faq from './documents/faq.js'
 import testimonial from './documents/testimonial.js'
-import settings from './documents/settings.js'
 
 export default [
 	product,
 	page,
 	faq,
-	testimonial,
-	settings
+	testimonial
 ]
