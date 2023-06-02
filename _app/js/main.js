51ceb589e33975101ac6965aa1e2640ddffbbc28
0yvz <oyvindg@live.no>
2023-06-02 05:18:12 +0200

    Imported testimonial function

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,7 +1,9 @@
 import titleunfocus from "./modules/titleunfocus.js";
 import shopCustomisation from "./modules/shop-customisation.js";
 import loadFAQSection from "./modules/faq.js";
+import loadTestimonialsSection from "./modules/testimonials.js";
 
 titleunfocus();
 shopCustomisation();
 loadFAQSection();
+loadTestimonialsSection();
